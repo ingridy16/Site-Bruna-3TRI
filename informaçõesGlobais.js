d73d91bcc70b737e4cb78d1dc384aad8605766c5
ingridy16 <106077050+ingridy16@users.noreply.github.com>
2024-11-07 22:01:16 -0300

    llll

--- informaçõesGlobais.js
+++ informaçõesGlobais.js
@@ -3,8 +3,15 @@ const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function visualisarinformacoesGlobais() {
     const res= await fetch(url)
     const dados= await res.json()
-    console.log(dados);
+    cosole.log(paragrafo);
+    const paragrafo document.creatElement('p')
+    paragrafo.classlist.add('graficos-conteiner__texto')
+    paragrafo.innerHTML= `Você sabia que o mundo tem &{dados.total_pessoas_mundo} no mundo e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
 
+
+
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo)
 }
 
 visualisarinformacoesGlobais()
