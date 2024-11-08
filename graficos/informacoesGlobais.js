bccc486b4c7310490236876114de42a2efa4d3ec
ingridy16 <106077050+ingridy16@users.noreply.github.com>
2024-11-07 22:15:14 -0300

    kjghv

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,8 +3,8 @@ const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function visualisarinformacoesGlobais() {
     const res= await fetch(url)
     const dados= await res.json()
-    cosole.log(paragrafo);
-    const paragrafo document.creatElement('p')
+    cosole.log(dados);
+    const paragrafo= document.creatElement('p')
     paragrafo.classlist.add('graficos-conteiner__texto')
     paragrafo.innerHTML= `Você sabia que o mundo tem &{dados.total_pessoas_mundo} no mundo e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
 
