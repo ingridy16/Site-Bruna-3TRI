38ef3d2f54377d215f003509aa358f85dce2b7d4
ingridy16 <106077050+ingridy16@users.noreply.github.com>
2024-11-07 23:26:59 -0300

    iohugyfvgh

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,4 +1,5 @@
 import { getCSS } from "./common.js";
+import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede(){
 const url= `https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json`
@@ -21,7 +22,36 @@ const res = await fetch(url)
 
   const layout = {
     plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color')
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+        text: 'Redes sociais com mais usuários no mundo',
+         x: 0,
+         font: {
+             color: getCSS('--primary-color'),
+             family: getCSS('--font'),
+             size: 30
+         }
+      },
+      xaxis: {
+        tickfont: tickConfig,
+        title: {
+            text: 'Nome das Redes Sociais',
+        font: {
+            color: getCSS('--secondary-color')
+        }
+        }
+      },
+      yaxis: {
+        tickfont: tickConfig,
+        tittle: {
+            text: 'Bilhões de usuários ativos',
+            font: {
+                color: getCSS('--secondary-color')
+            }
+        }
+      
+      }
+    
 
 }
 
