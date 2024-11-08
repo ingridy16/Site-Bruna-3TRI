67be102df9386151969e0b6beb72df4780cfd633
ingridy16 <106077050+ingridy16@users.noreply.github.com>
2024-11-07 22:11:18 -0300

    fffff

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,8 +8,6 @@ async function visualisarinformacoesGlobais() {
     paragrafo.classlist.add('graficos-conteiner__texto')
     paragrafo.innerHTML= `Você sabia que o mundo tem &{dados.total_pessoas_mundo} no mundo e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
 
-
-
 const container = document.getElementById('graficos-container')
 container.appendChild(paragrafo)
 }
